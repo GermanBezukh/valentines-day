d0663f3775d56ca543071b7c7af4cdad636a69a0
GermanBezukh <158227390+GermanBezukh@users.noreply.github.com>
2024-01-30 22:00:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+    document.getElementById('response').innerText = "See you on February 14th.";
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    document.getElementById('response').innerText = "No is not an option, I will see you on February 14th.";
+});
