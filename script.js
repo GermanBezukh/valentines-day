eee084a603ff0960ba63a354c9fd97c542ede809
GermanBezukh <158227390+GermanBezukh@users.noreply.github.com>
2024-01-30 22:18:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
     document.getElementById('response').innerText = "See you on February 14th.";
+    document.getElementById('catImage').src = "cat2.jpg"; // Change image to new image for "Yes"
 });
 
 document.getElementById('noBtn').addEventListener('click', function() {
     document.getElementById('response').innerText = "No is not an option, I will see you on February 14th.";
+    document.getElementById('catImage').src = "cat3.jpg"; // Change image to new image for "No"
 });
